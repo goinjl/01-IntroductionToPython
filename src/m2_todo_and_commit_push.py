1b07aadfeead36d8bc74f345f5def62030b713a4
goinjl <goinjl@rose-hulman.edu>
2019-01-17 09:39:43 -0500

    Hey!

--- src/m2_todo_and_commit_push.py
+++ src/m2_todo_and_commit_push.py
@@ -76,7 +76,7 @@ print('Jacey')
 #   This one should print the *product* of 3,607 and 34,227.
 #   Let the computer do the arithmetic for you (no calculators!).
 #   You do NOT have to use strings for this, so no quotation marks!
-#
+print(3607 * 34227)
 #   TEST your code by re-running this module, then asking someone
 #   whom you trust:
 #      What number did your   print   display for this TO-DO?
